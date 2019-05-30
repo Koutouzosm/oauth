fdaf51a09bfa4ee9b762aaa834e4bc7a0f362c33
Mano <Koutouzosm@gmail.com>
2019-05-30 18:03:27 -0400

    set up routes and passport google auth

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// this file goes into <div className="gitignore"
+
+module.exports = {
+    google:{
+        clientID:'639421230376-0u4fk1k6jsropdd0vv090a915ik4ru9k.apps.googleusercontent.com',
+        clientSecret:'vD-GrZ-jw_y6xEHes0RmMs07'
+    }
+}
